943f9aaf63163a53f8d1d1ec8d8dc15482365c90
Mattexssd <matteo07.ricci@gmail.com>
2025-02-05 18:57:05 +0100

    Add files via upload

---
+++ funzioni.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <bitset>
+#include "funzioni.h"
+
+using namespace std;
+
+string charToBin(char c) {
+    return bitset<8>(c).to_string();
+}
+
+string stringToBin(const string& str) {
+    string binary;
+    for (char c : str) {
+        binary += charToBin(c) + " ";
+    }
+    return binary;
+}
+
+string leggiMessaggio(){
+    ifstream file("messaggio.txt");
+    if (!file) {
+        cout << "Errore nell'apertura del file " <<endl;
+        return "";
+    }
+    string message, line;
+    while (getline(file, line)) {
+        message += line + "\n";
+    }
+    file.close();
+    return message;
+}
+
+void scritturaMessaggio(string& binaryMessage) {
+    ofstream file("frame.txt");
+    if (!file) {
+        cout << "Errore nella scrittura del file " << "frame.txt" << endl;
+        return;
+    }
+    file << binaryMessage;
+    file.close();
+}
+
+IpHeader creaDatagram() {
+    IpHeader header;
+    header.versione;
+    header.headerLength;
+    header.tipoServizio;
+    header.totalLenght;
+    header.identificatore;
+    header.flag;
+    header.offset;
+    header.ttl;
+    header.protocollo;
+    header.checksum;
+    header.indirizzoMit;
+    header.indirizzoDest;
+    return header;
+}
+int main() {
+    string input;
+    cin>>input;
+    string output = stringToBin(input);
+    string binario;
+    cout << "Stringa originale: " << input << endl;
+    cout << "Stringa in binario: " << output << endl;
+    leggiMessaggio();
+    scritturaMessaggio(binario);
+}
