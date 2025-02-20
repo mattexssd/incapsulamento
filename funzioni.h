cfab888b166bc78c48f5f8f801b588c3b89af759
Mattexssd <matteo07.ricci@gmail.com>
2025-02-20 11:15:06 +0100

    Update funzioni.h

--- funzioni.h
+++ funzioni.h
@@ -8,21 +8,34 @@ void scritturaMessaggio(const string& nomeFile, const string& contenuto);
 string charToBin(char c);
 string stringToBin(const string& str);
 string binarioAStringa(const string& bin);
+
 //struct IpHeader
 struct IpHeader {
-    string versione;
-    string headerLength;
-    string typeOfService;
-    string totalLenght;
-    string identificatore;
-    string flag;
-    string offset;
-    string timeToLive;
-    string protocollo;
-    string checksum;
-    string indirizzoMit;
-    string indirizzoDest;
-    void creaDatagram();
+    string dsap;
+    string ssap;
+    int ethertype;
+    int vers;
+    int hlen;
+    int precedence;
+    int delay;
+    int throughput;
+    int reliability;
+    int cost;
+    int total_length;
+    int id;
+    int df;
+    int mf;
+    int fragment_offset;
+    int ttl;
+    int protocol;
+    string header_checksum;
+    string source_ip;
+    string dest_ip;
+    string copy_flag;
+    string option_class;
+    string option_number;
+    creaDatagram();
 };
 
 #endif
+
